4f34fa243d657691de163389cd6e0e49631a2b26
John Fox <john.fox@capgemini.com>
2026-07-02 11:28:48 +0100

    .Net upgrade

--- test/Defra.Trade.Events.DAERA.GCNotifier.Tests/Functions/GcNotificationSubscriberServiceBusTriggerFunctionTests.cs
+++ test/Defra.Trade.Events.DAERA.GCNotifier.Tests/Functions/GcNotificationSubscriberServiceBusTriggerFunctionTests.cs
@@ -2,16 +2,14 @@
 // Licensed under the Open Government License v3.0.
 
 using Azure.Messaging.ServiceBus;
-using Defra.Trade.Common.Functions;
-using Defra.Trade.Common.Functions.Interfaces;
+using Defra.Trade.Common.Functions.Isolated;
+using Defra.Trade.Common.Functions.Isolated.Interfaces;
 using Defra.Trade.Events.DAERA.GCNotifier.Application.Dtos.Inbound;
 using Defra.Trade.Events.DAERA.GCNotifier.Application.Models;
+using Defra.Trade.Events.DAERA.GCNotifier.Application.Services.Contracts;
 using FakeItEasy;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.ServiceBus;
+using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
-
-using ExecutionContext = Microsoft.Azure.WebJobs.ExecutionContext;
 
 namespace Defra.Trade.Events.DAERA.GCNotifier.Functions;
 
@@ -19,15 +17,18 @@ public sealed class GcNotificationSubscriberServiceBusTriggerFunctionTests
 {
     private readonly IBaseMessageProcessorService<GCNotificationInbound> _processor;
     private readonly IMessageRetryService _retry;
+    private readonly IQueueClientFactory _queueClientFactory;
+    private readonly ILogger<GcNotificationSubscriberServiceBusTriggerFunction> _logger;
     private readonly GcNotificationSubscriberServiceBusTriggerFunction _sut;
-    private static readonly string[] _messageArgs = ["123"];
 
     public GcNotificationSubscriberServiceBusTriggerFunctionTests()
     {
         _processor = A.Fake<IBaseMessageProcessorService<GCNotificationInbound>>(opt => opt.Strict());
         _retry = A.Fake<IMessageRetryService>(opt => opt.Strict());
+        _queueClientFactory = A.Fake<IQueueClientFactory>();
+        _logger = A.Fake<ILogger<GcNotificationSubscriberServiceBusTriggerFunction>>();
 
-        _sut = new GcNotificationSubscriberServiceBusTriggerFunction(_processor, _retry);
+        _sut = new GcNotificationSubscriberServiceBusTriggerFunction(_processor, _retry, _queueClientFactory, _logger);
     }
 
     [Fact]
@@ -35,49 +36,43 @@ public sealed class GcNotificationSubscriberServiceBusTriggerFunctionTests
     {
         // arrange
         string messageId = Guid.NewGuid().ToString();
-        var invocationId = Guid.NewGuid();
+        string invocationId = Guid.NewGuid().ToString();
         string functionName = Guid.NewGuid().ToString();
         var message = ServiceBusModelFactory.ServiceBusReceivedMessage(messageId: messageId, body: BinaryData.FromString("{\"GCId\": \"123\"}"));
 
         var actions = A.Fake<ServiceBusMessageActions>(opt => opt.Strict());
-        var context = new ExecutionContext { InvocationId = invocationId, FunctionName = functionName };
-        var eventStore = A.Fake<IAsyncCollector<ServiceBusMessage>>(opt => opt.Strict());
-        var retryQueue = A.Fake<IAsyncCollector<ServiceBusMessage>>(opt => opt.Strict());
-        var logger = A.Fake<ILogger>(opt => opt.Strict());
+        var context = A.Fake<FunctionContext>();
+        var functionDefinition = A.Fake<FunctionDefinition>();
+        A.CallTo(() => context.InvocationId).Returns(invocationId);
+        A.CallTo(() => context.FunctionDefinition).Returns(functionDefinition);
+        A.CallTo(() => functionDefinition.Name).Returns(functionName);
 
-        var setRetryContext = A.CallTo(() => _retry.SetContext(message, retryQueue));
+        var retrySender = A.Fake<ServiceBusSender>();
+        A.CallTo(() => _queueClientFactory.CreateNotifierQueueClient()).Returns(retrySender);
+
+        var setRetryContext = A.CallTo(() => _retry.SetContext(message, retrySender));
         var processAsyncCall = A.CallTo(() => _processor.ProcessAsync(
-            invocationId.ToString(),
+            invocationId,
             GcNotificationSubscriberSettings.DefaultQueueName,
             GcNotificationSubscriberSettings.PublisherId,
             message,
             actions,
-            eventStore,
             null,
             null,
             GcNotificationSubscriberSettings.PublisherId,
             GcNotificationSubscriberSettings.DefaultQueueName,
             "Create"
         ));
-        var loggerStart = LoggerFakeHelper.LoggerCall(logger, LogLevel.Information, 0, null, "Messages Id : {MessageId} received on {FunctionName}", () => new[] { messageId, functionName });
-        var loggerReceived = LoggerFakeHelper.LoggerCall(logger, LogLevel.Information, 0, null, "Message received with GcId: {GcId}", () => _messageArgs);
-        var loggerEnd = LoggerFakeHelper.LoggerCall(logger, LogLevel.Information, 0, null, "Finished processing Messages Id : {MessageId} received on {FunctionName}", () => new[] { messageId, functionName });
 
         processAsyncCall.Returns(true);
-        loggerStart.DoesNothing();
-        loggerReceived.DoesNothing();
-        loggerEnd.DoesNothing();
         setRetryContext.DoesNothing();
 
         // act
-        await _sut.RunAsync(message, actions, context, eventStore, retryQueue, logger);
+        await _sut.RunAsync(message, actions, context);
 
         // assert
-        setRetryContext.MustHaveHappenedOnceExactly()
-            .Then(loggerStart.MustHaveHappenedOnceExactly())
-            .Then(loggerReceived.MustHaveHappenedOnceExactly())
-            .Then(processAsyncCall.MustHaveHappenedOnceExactly())
-            .Then(loggerEnd.MustHaveHappenedOnceExactly());
+        setRetryContext.MustHaveHappenedOnceExactly();
+        processAsyncCall.MustHaveHappenedOnceExactly();
     }
 
     [Fact]
@@ -85,47 +80,44 @@ public sealed class GcNotificationSubscriberServiceBusTriggerFunctionTests
     {
         // arrange
         string messageId = Guid.NewGuid().ToString();
-        var invocationId = Guid.NewGuid();
+        string invocationId = Guid.NewGuid().ToString();
         string functionName = Guid.NewGuid().ToString();
         var message = ServiceBusModelFactory.ServiceBusReceivedMessage(messageId: messageId);
         var actions = A.Fake<ServiceBusMessageActions>(opt => opt.Strict());
-        var context = new ExecutionContext { InvocationId = invocationId, FunctionName = functionName };
-        var eventStore = A.Fake<IAsyncCollector<ServiceBusMessage>>(opt => opt.Strict());
-        var retryQueue = A.Fake<IAsyncCollector<ServiceBusMessage>>(opt => opt.Strict());
-        var logger = A.Fake<ILogger>(opt => opt.Strict());
+        var context = A.Fake<FunctionContext>();
+        var functionDefinition = A.Fake<FunctionDefinition>();
+        A.CallTo(() => context.InvocationId).Returns(invocationId);
+        A.CallTo(() => context.FunctionDefinition).Returns(functionDefinition);
+        A.CallTo(() => functionDefinition.Name).Returns(functionName);
+
+        var retrySender = A.Fake<ServiceBusSender>();
+        A.CallTo(() => _queueClientFactory.CreateNotifierQueueClient()).Returns(retrySender);
+
         var exception = new Exception("abc");
 
-        var setRetryContext = A.CallTo(() => _retry.SetContext(message, retryQueue));
+        var setRetryContext = A.CallTo(() => _retry.SetContext(message, retrySender));
         var processAsyncCall = A.CallTo(() => _processor.ProcessAsync(
-            invocationId.ToString(),
+            invocationId,
             GcNotificationSubscriberSettings.DefaultQueueName,
             GcNotificationSubscriberSettings.PublisherId,
             message,
             actions,
-            eventStore,
             null,
             null,
             GcNotificationSubscriberSettings.PublisherId,
             GcNotificationSubscriberSettings.DefaultQueueName,
             "Create"
         ));
-        var loggerStart = LoggerFakeHelper.LoggerCall(logger, LogLevel.Information, 0, null, "Messages Id : {MessageId} received on {FunctionName}", () => new[] { messageId, functionName });
-        var loggerNoGcId = LoggerFakeHelper.LoggerCall(logger, LogLevel.Warning, 0, null, "The incoming message does not have a GcId");
-        var loggerError = LoggerFakeHelper.LoggerCall(logger, LogLevel.Critical, 0, exception, "abc");
 
-        processAsyncCall.Returns(true);
-        loggerNoGcId.DoesNothing();
-        loggerStart.Throws(exception);
-        loggerError.DoesNothing();
+        processAsyncCall.Throws(exception);
         setRetryContext.DoesNothing();
 
         // act
-        await _sut.RunAsync(message, actions, context, eventStore, retryQueue, logger);
+        await _sut.RunAsync(message, actions, context);
 
         // assert
-        setRetryContext.MustHaveHappenedOnceExactly()
-            .Then(loggerStart.MustHaveHappenedOnceExactly())
-            .Then(loggerError.MustHaveHappenedOnceExactly());
+        setRetryContext.MustHaveHappenedOnceExactly();
+        processAsyncCall.MustHaveHappenedOnceExactly();
     }
 
     [Fact]
@@ -133,48 +125,42 @@ public sealed class GcNotificationSubscriberServiceBusTriggerFunctionTests
     {
         // arrange
         string messageId = Guid.NewGuid().ToString();
-        var invocationId = Guid.NewGuid();
+        string invocationId = Guid.NewGuid().ToString();
         string functionName = Guid.NewGuid().ToString();
         var message = ServiceBusModelFactory.ServiceBusReceivedMessage(messageId: messageId, body: BinaryData.FromString("{\"AProperty\": \"123\"}"));
 
         var actions = A.Fake<ServiceBusMessageActions>(opt => opt.Strict());
-        var context = new ExecutionContext { InvocationId = invocationId, FunctionName = functionName };
-        var eventStore = A.Fake<IAsyncCollector<ServiceBusMessage>>(opt => opt.Strict());
-        var retryQueue = A.Fake<IAsyncCollector<ServiceBusMessage>>(opt => opt.Strict());
-        var logger = A.Fake<ILogger>(opt => opt.Strict());
+        var context = A.Fake<FunctionContext>();
+        var functionDefinition = A.Fake<FunctionDefinition>();
+        A.CallTo(() => context.InvocationId).Returns(invocationId);
+        A.CallTo(() => context.FunctionDefinition).Returns(functionDefinition);
+        A.CallTo(() => functionDefinition.Name).Returns(functionName);
 
-        var setRetryContext = A.CallTo(() => _retry.SetContext(message, retryQueue));
+        var retrySender = A.Fake<ServiceBusSender>();
+        A.CallTo(() => _queueClientFactory.CreateNotifierQueueClient()).Returns(retrySender);
+
+        var setRetryContext = A.CallTo(() => _retry.SetContext(message, retrySender));
         var processAsyncCall = A.CallTo(() => _processor.ProcessAsync(
-            invocationId.ToString(),
+            invocationId,
             GcNotificationSubscriberSettings.DefaultQueueName,
             GcNotificationSubscriberSettings.PublisherId,
             message,
             actions,
-            eventStore,
             null,
             null,
             GcNotificationSubscriberSettings.PublisherId,
             GcNotificationSubscriberSettings.DefaultQueueName,
             "Create"
         ));
-        var loggerStart = LoggerFakeHelper.LoggerCall(logger, LogLevel.Information, 0, null, "Messages Id : {MessageId} received on {FunctionName}", () => new[] { messageId, functionName });
-        var loggerNoGcId = LoggerFakeHelper.LoggerCall(logger, LogLevel.Warning, 0, null, "The incoming message does not have a GcId");
-        var loggerEnd = LoggerFakeHelper.LoggerCall(logger, LogLevel.Information, 0, null, "Finished processing Messages Id : {MessageId} received on {FunctionName}", () => new[] { messageId, functionName });
 
         processAsyncCall.Returns(true);
-        loggerStart.DoesNothing();
-        loggerNoGcId.DoesNothing();
-        loggerEnd.DoesNothing();
         setRetryContext.DoesNothing();
 
         // act
-        await _sut.RunAsync(message, actions, context, eventStore, retryQueue, logger);
+        await _sut.RunAsync(message, actions, context);
 
         // assert
-        setRetryContext.MustHaveHappenedOnceExactly()
-            .Then(loggerStart.MustHaveHappenedOnceExactly())
-            .Then(loggerNoGcId.MustHaveHappenedOnceExactly())
-            .Then(processAsyncCall.MustHaveHappenedOnceExactly())
-            .Then(loggerEnd.MustHaveHappenedOnceExactly());
+        setRetryContext.MustHaveHappenedOnceExactly();
+        processAsyncCall.MustHaveHappenedOnceExactly();
     }
 }
